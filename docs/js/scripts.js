4a4ab0f116c861eb4652939666c56f64f83fd9f1
Sam Foxman <elasticvine@protonmail.com>
2021-07-29 15:46:59 -0400

    Start site

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -3,5 +3,8 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Highlights current date on contact page
+window.addEventListener('DOMContentLoaded', event => {
+    const listHoursArray = document.body.querySelectorAll('.list-hours li');
+    listHoursArray[new Date().getDay()].classList.add(('today'));
+})
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,5 @@
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Highlights current date on contact page
+window.addEventListener('DOMContentLoaded', event => {
+    const listHoursArray = document.body.querySelectorAll('.list-hours li');
+    listHoursArray[new Date().getDay()].classList.add(('today'));
+})
