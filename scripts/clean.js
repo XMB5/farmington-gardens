08de229e22f6d7b3fbd73679862bb8c0a34f97ed
Sam Foxman <elasticvine@protonmail.com>
2021-07-29 14:34:31 -0400

    Keep CNAME file

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,5 +3,10 @@ const upath = require('upath');
 
 const destPath = upath.resolve(upath.dirname(__filename), '../docs');
 
-sh.rm('-rf', `${destPath}/*`)
-
+const files = sh.ls(`${destPath}`)
+sh.set('-f');  // prevent globbing accidents, see https://github.com/shelljs/shelljs/wiki/Security-guidelines
+for (let file of files) {
+    if (file !== 'CNAME') {
+        sh.rm('-rf', '--', upath.join(destPath, file));
+    }
+}
